ce5f7e23a1d6ddd24e89c44d7e7076c264f4cfbe
jack <847749593@qq.com>
2022-12-29 11:59:17 +0800

    build: server add bodyParser

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const bodyParser = require('body-parser')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controller/errorController')
 
 const app = express()
 
 app.use(morgan('common'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.all('*', (req, res, next) => {
   next(new AppError(`找不到路径：${req.originalUrl}`, 404))
